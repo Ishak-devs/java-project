1e7ef06763f9e2a1fe37ccc6d86123a5926af9e7
ishak <i.kouici@insta.fr>
2025-08-22 19:20:00 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import com.sun.tools.javac.Main;
+
+class Produits {
+    String ordinateur;
+    Int prix;
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
+
