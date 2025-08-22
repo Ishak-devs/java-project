91640ebe158f0111fedb8243e05a39d6bbc46ff4
ishak <i.kouici@insta.fr>
2025-08-22 19:31:38 +0200

    add nom et prix

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,9 @@ int prix;
 
     public static void main(String[] args){
         produits p1 = new produits();
+        p1.nom = "ordinateur";
+        p1.prix = 125;
 
-        System.out.println("hello");
+        System.out.println(p1.nom + p1.prix);
     }
         }
